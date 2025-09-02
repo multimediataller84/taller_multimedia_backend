24c928ffa0f1b3899918e11b455109953afffb1d
MarcoGamer304 <marcoeli30@gmail.com>
2025-09-02 02:13:13 -0600

    fix: add authMiddleware on User module

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -16,8 +16,8 @@ router.get("/openapi.json", (req, res) => {
   res.send(swaggerSpec);
 });
 
-router.use("/user", usersRouter);
 router.use("/auth", authRouter);
+router.use("/user", authMiddleware, usersRouter);
 router.use("/customer", authMiddleware, customerRouter);
 router.use("/invoice", authMiddleware, invoiceRouter);
 router.use("/invoice/detail", authMiddleware, invoiceDetailRouter);
