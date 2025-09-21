52867af3ec8f7ccc33560a7b0be4f932d3ddbd12
MarcoGamer304 <marcoeli30@gmail.com>
2025-09-21 17:20:55 -0600

    fix role middleware

--- src/middlewares/role.ts
+++ src/middlewares/role.ts
@@ -1,13 +1,29 @@
 import { type Request, type Response, type NextFunction } from "express";
+import jwt from "jsonwebtoken";
 
-export const verifyRole = (role: string) => {
+export const verifyRole = (role: string[]) => {
   return (req: Request, res: Response, next: NextFunction) => {
-    const user = (req as any).user;
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).json({ error: "Token missing" });
+    }
+
+    const token = authHeader.split(" ")[1];
+    if (!token) {
+      return next(new Error("Token missing"));
+    }
+
+    const user = jwt.decode(token);
+    if (!user || typeof user !== "object") {
+      return res.status(401).json({ error: "Invalid token" });
+    }
+
     if (!user) {
       return res.status(401).json({ error: "User not authenticated" });
     }
 
-    if (user.role !== role) {
+    if (!role.includes(user.role)) {
       return res.status(403).json({ error: "Forbidden: insufficient role" });
     }
 
